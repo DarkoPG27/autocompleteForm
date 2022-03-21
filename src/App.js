15a0b05b20b4bda2e5d60f609d970d20e2dbfd9f
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-03-21 22:15:25 +0100

    added App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
 import './App.css';
+import { Table, } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(`https://restcountries.com/v2/name/${term}`);
+
+        const data = await response.json();
+        const transformedData = data.map((country) => {
+          return {
+            name: country.name,
+            population: country.population,
+            capital: country.capital,
+            region: country.region,
+            key: country.name
+          };
+        });
+        /* const filteredData = transformedData.filter((country) => {
+          return country.name.includes(term);
+        }); */
+        console.log(transformedData);
+        setCountries(transformedData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (term) {
+      const timeoutId = setTimeout(() => {
+        fetchCountries();
+      }, 500);
+      console.log(timeoutId);
+      return () => {
+        console.log(timeoutId, "cleared");
+        clearTimeout(timeoutId);
+      };
+    } else {
+      setCountries([]);
+    }
+  }, [term]);
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Container className="App">
+
+      <h1>Countries Autocomplete</h1>
+      <input value={term} onChange={(event) => setTerm(event.target.value)} />
+      <Table responsive="md" bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Capital</th>
+            <th>Region</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        {countries.map((country) => {
+          return (
+            <tbody>
+              <tr>
+                <th key={country.key}>{country.name}</th>
+                <th>{country.capital}</th>
+                <th>{country.region}</th>
+                <th>{country.population}</th>
+              </tr>
+            </tbody>
+          );
+        })} </Table>
+    </Container>
   );
 }
-
-export default App;
